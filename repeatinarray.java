307dec83a2219979bce890ec41af77ee94e5e23b
Srinath Karthikeyan <sriinathk@gmail.com>
2016-08-12 21:00:46 +0530

    Create repeatinarray.java

---
+++ repeatinarray.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class registernumber
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int[] a=new int[n];
+    for(int i=0;i<n;i++)
+        {
+        a[i]=s.nextInt();
+    }
+    for(int i=0;i<n;i++)
+        {
+        int count=0;
+        for(int j=0;j<n;j++)
+            {
+        if(a[i]==a[j])
+            count++;}
+         if(count==1)
+             {
+            System.out.println(a[i]);
+             break;
+    }
+}
+}
+}
